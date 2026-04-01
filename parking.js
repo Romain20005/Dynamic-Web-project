7328228a41e87fd12ab995292eea20525c5c2b51
Romain20005 <romain.musungayi@student.ehb.be>
2026-04-01 12:41:27 +0200

    Afbeeldingen

--- parking.js
+++ parking.js
@@ -1,4 +1,7 @@
-'use strict'
+'use strict';
 async function dataophalen() { const response = await 
     fetch ('/api/explore/v2.1/catalog/datasets/bruxelles_parkings_publics/records?limit=20')
+    const data = await response.json();
+    console.log(data)
 }
+dataophalen
