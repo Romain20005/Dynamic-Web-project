c0645e3d43777e3fa37bf7fb6ee64f7a686ee6e2
Romain20005 <romain.musungayi@student.ehb.be>
2026-04-01 14:39:51 +0200

    Correcte api link

--- parking.js
+++ parking.js
@@ -1,6 +1,6 @@
 'use strict';
 async function dataophalen() { const response = await 
-    fetch ('/api/explore/v2.1/catalog/datasets/bruxelles_parkings_publics/records?limit=20')
+    fetch ('https://opendata.brussels.be/explore/embed/dataset/bruxelles_parkings_publics/table/?disjunctive.name_nl&disjunctive.commune_gemeente&sort=name_nl')
     const data = await response.json();
     console.log(data); 
 }
