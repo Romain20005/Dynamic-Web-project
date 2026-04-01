169a1dc95f9e1e459aa21549025644e255f6a6c0
Ben-Fodda <ilias.benfodda@student.odisee.be>
2026-04-01 19:02:44 +0200

    Succesvol data ophalen via api

--- parking.js
+++ parking.js
@@ -1,7 +1,26 @@
 'use strict';
-async function dataophalen() { const response = await 
-    fetch ('/api/explore/v2.1/catalog/datasets/bruxelles_parkings_publics/records?limit=20')
-    const data = await response.json();
-    console.log(data); 
+const API_URL = "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/bruxelles_parkings_publics/records?limit=20";
+async function getParkings() {
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+
+        const tableBody = document.querySelector("#parkingTable tbody");
+
+        tableBody.innerHTML = data.results.map(p => `
+            <tr>
+                <td>${p.name_nl}</td>
+                <td>${p.adres_}</td>
+                <td>${p.operator_fr}</td>
+                <td>${p.capacity}</td>
+                <td>${p.maxheight}</td>
+                <td>${p.commune_gemeente}</td>
+            </tr>
+        `).join("");
+
+    } catch (error) {
+        console.error("Fout bij ophalen parkings:", error);
+    }
 }
-dataophalen();
+
+getParkings();
